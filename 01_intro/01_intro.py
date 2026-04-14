012cd0634881c3a2512e24c75066731fd2761db6
shubham-2122 <shubhamjadav.tops@gmail.com>
2026-04-14 14:08:29 +0530

    python

---
+++ 01_intro/01_intro.py
@@ -0,0 +1,9 @@
+print("welcome to python")
+
+## print is function : block of code call it
+
+print("krish panchal",end=" as develop")
+#print("as python developer")
+
+#print("krish panchal",end=" ")
+#print("as python developer")
---
+++ 02_varibale/01_varible.py
@@ -0,0 +1,36 @@
+'''
+    varibale : it's simple type of varibale
+             : it;s store a value in single varibale
+
+    syntax :
+        varibname = value
+
+    Rules :
+        1) do not start with number (12name)
+        2) do not start with symbole (#$shjkh)
+        3) do not start with keyword (print)
+        4) lower cas and uppaer thae deffient
+'''
+# int value
+a = 10
+print("A :",a)
+print(type(a))
+
+print("****************")
+# float value 45.45
+num = 46.8
+print("num :",num)
+print(type(num))
+
+print("****************")
+
+name = "krish pachanl"
+print("Name :",name)
+print(type(name))
+
+
+
+
+
+
+
